c534a7b8b239fb25986ced29baa4034c35633930
ahsan1800411 <ahsanmumtaz008976@gmail.com>
2021-12-15 05:42:35 +0500

    Initail Commit

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,7 +1,8 @@
 const User = require('../models/User');
-// const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-// create a user
+// create/register a user
 
 exports.newUser = async (req, res) => {
   const { email, password, name } = req.body;
@@ -12,8 +13,42 @@ exports.newUser = async (req, res) => {
     password,
     name,
   });
+
+  const token = jwt.sign({ email }, process.env.JWT_SECRET_KEY, {
+    expiresIn: 360000,
+  });
+
   await newUser.save();
   res.json({
     message: 'User created sucessfully',
+    token,
+  });
+};
+
+// login a user;
+
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: 'Please fill the required fields' });
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(400).json({ message: 'User not found' });
+  }
+
+  //  match the password against the hashed password
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(400).json({ message: 'Wrong Credentials' });
+  }
+  const token = jwt.sign({ email }, process.env.JWT_SECRET_KEY, {
+    expiresIn: 360000,
+  });
+
+  return res.status(200).json({
+    message: 'User successfully logged in',
+    token,
   });
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,8 @@
-const { newUser } = require('../controllers/userControllers');
+const { newUser, loginUser } = require('../controllers/userControllers');
 
 const router = require('express').Router();
 
-router.route('/create').post(newUser);
+router.route('/register').post(newUser);
+router.route('/login').post(loginUser);
 
 module.exports = router;
